221639ba64842bbda0be9686415ccdf6a8036a3c
Qian <161250102@smail.nju.edu.cn>
2018-09-08 11:50:51 +0800

    wxPython + pygame 初步整合完成 //下午增加新功能

--- Tk_main.py
+++ Tk_main.py
@@ -1,15 +1,53 @@
+# coding=utf-8
 import wx
 import os
 import thread
 import pygame
+import cv2 as cv
+
+
+class SubScreen(object):
+
+    def __init__(self, size):
+        self.points = []
+        self.color = (0, 0, 0)
+        self.width = 1
+        self.screen = pygame.Surface(size, pygame.SRCALPHA, 32)
+        self.screen.fill((255, 255, 255, 0))
 
 
 class SDLThread:
-    def __init__(self, screen):
+    def __init__(self, size):
+        self.size = size
         self.m_bKeepGoing = self.m_bRunning = False
-        self.screen = screen
-        self.color = (255, 0, 0)
-        self.rect = (10, 10, 100, 100)
+        self.screen = pygame.display.set_mode(size)
+        self.screen.fill((255, 255, 255))
+        self.screen_list = []
+        self.active_screen = SubScreen(size)
+        self.draw_flag = False
+
+    def handle(self, img):
+        # 二值化图像
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        ret, binary = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
+
+        out_binary, contours, hierarchy = cv.findContours(binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        for cnt in range(len(contours)):
+            # 轮廓逼近
+            epsilon = 0.03 * cv.arcLength(contours[cnt], True)
+            approx = cv.approxPolyDP(contours[cnt], epsilon, True)
+
+            # 分析几何形状
+            corners = len(approx)
+            if corners == 3:
+                return "三角形"
+            if corners == 4:
+                return "矩形"
+            if corners >= 7:
+                return "圆形"
+            if 4 < corners < 7:
+                return "多边形"
+        return ""
 
     def Start(self):
         self.m_bKeepGoing = self.m_bRunning = True
@@ -23,13 +61,34 @@ class SDLThread:
 
     def Run(self):
         while self.m_bKeepGoing:
-            e = pygame.event.poll()
-            if e.type == pygame.MOUSEBUTTONDOWN:
-                self.color = (255,0,128)
-                self.rect = (e.pos[0], e.pos[1], 100, 100)
-                print e.pos
-            self.screen.fill((0,0,0))
-            self.screen.fill(self.color,self.rect)
+            event = pygame.event.poll()
+            if event.type == pygame.QUIT:
+                return
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # 鼠标单击左键
+                if event.button == 1:
+                    if self.draw_flag:
+                        self.draw_flag = False
+                        pygame.draw.lines(self.active_screen.screen, (0, 0, 0), True, self.active_screen.points)
+                    else:
+                        self.draw_flag = True
+                elif event.button == 3:
+                    if len(self.active_screen.points) > 1:
+                        self.screen_list.append(self.active_screen)
+                        pygame.image.save(self.active_screen.screen, "s.png")
+                        img = cv.imread("s.png")
+                        print self.handle(img)
+                        self.active_screen = SubScreen(self.size)
+            elif event.type == pygame.MOUSEMOTION:
+                (x, y) = pygame.mouse.get_pos()
+                if self.draw_flag and y < self.size[1]:
+                    self.active_screen.points.append((x, y))
+
+            if len(self.active_screen.points) > 1:
+                pygame.draw.lines(self.active_screen.screen, self.active_screen.color, not self.draw_flag, self.active_screen.points)
+            for s in self.screen_list:
+                self.screen.blit(pygame.Surface.convert_alpha(s.screen), (0, 0))
+            self.screen.blit(pygame.Surface.convert_alpha(self.active_screen.screen), (0, 0))
             pygame.display.flip()
         self.m_bRunning = False
 
@@ -41,8 +100,7 @@ class SDLPanel(wx.Panel):
         os.environ['SDL_WINDOWID'] = str(self.GetHandle())
         os.environ['SDL_VIDEODRIVER'] = 'windib'
         pygame.display.init()
-        window = pygame.display.set_mode(tplSize)
-        self.thread = SDLThread(window)
+        self.thread = SDLThread(tplSize)
         self.thread.Start()
 
     def __del__(self):
@@ -53,7 +111,7 @@ class MyFrame(wx.Frame):
     def __init__(self, parent, ID, strTitle, tplSize, win_size):
         wx.Frame.__init__(self, parent, ID, strTitle, size=win_size)
         self.pnlSDL = SDLPanel(self, -1, tplSize)
-        self.user_box = wx.TextCtrl(self, -1, u'161250102', pos=(0, 600), size=(200, 50))
+        self.user_box = wx.TextCtrl(self, -1, u'161250102', pos=(10, 620), size=(200, 30))
         # self.Fit()
 
 
