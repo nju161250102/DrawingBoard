f6f4886937d839b205406b39485f735c6e210034
Qian <161250102@smail.nju.edu.cn>
2018-09-12 22:49:15 +0800

    重排按钮文本框，支持标注更新

--- Main.py
+++ Main.py
@@ -19,14 +19,20 @@ class SDLThread:
         self.current_num = 0
         self.state = 0  # 0-等待绘制 1-正在绘制 2-选择模式
         self.panel.identify_button.Bind(wx.EVT_BUTTON, self.get_shape)
-        self.panel.choose_button.Bind(wx.EVT_BUTTON, self.choose)
+        self.panel.choose_button.Bind(wx.EVT_BUTTON, self.choose_event)
+        self.panel.draw_button.Bind(wx.EVT_BUTTON, self.draw_event)
 
     def add_point(self, pos):
         if len(self.lines) > 0:
             self.lines[-1].append(pos)
 
-    def choose(self, event):
+    def choose_event(self, event):
         self.state = 2
+
+    def draw_event(self, event):
+        self.state = 0
+        self.current_num = len(self.shapes)
+        self.panel.set_text("请绘制图形", "")
 
     def draw_lines(self, screen):
         for line in self.lines:
@@ -43,6 +49,8 @@ class SDLThread:
         return (0, 0 ,0)
 
     def get_shape(self, event):
+        if self.state != 0 or len(self.lines) == 0:
+            return
         screen = pygame.Surface(self.size, pygame.SRCALPHA, 32)
         screen.fill((255, 255, 255, 0))
         self.draw_lines(screen)
@@ -71,7 +79,11 @@ class SDLThread:
                 y_list.sort()
                 pos = ((x_list[0] + x_list[1]) / 2, (y_list[0] + y_list[1]) / 2)
                 size = ((x_list[2] + x_list[3]) / 2 - pos[0], (y_list[2] + y_list[3]) / 2 - pos[1])
-                self.shapes.append(Model.Rect(pos, size, self.get_color(), self.panel.get_input()))
+                if float(abs(size[0] - size[1])) / max(size[0], size[1]) < 0.1:
+                    self.shapes.append(Model.Square(pos, (size[0] + size[1]) / 2,
+                                                    self.get_color(), self.panel.get_input()))
+                else:
+                    self.shapes.append(Model.Rect(pos, size, self.get_color(), self.panel.get_input()))
             elif corners >= 7:
                 mm = cv.moments(contours[cnt])
                 cx = int(mm['m10'] / mm['m00'])
@@ -82,7 +94,7 @@ class SDLThread:
         self.lines = []
         self.current_num = len(self.shapes)
         self.main_screen.fill((255, 255, 255))
-        self.panel.set_text(self.shapes[-1].get_shape(), "")
+        self.panel.set_text("识别结果：" + self.shapes[-1].get_shape(), "")
 
     def run(self):
         while True:
@@ -106,7 +118,7 @@ class SDLThread:
                         for index, shape in enumerate(self.shapes):
                             if shape.judge_point(pos):
                                 self.current_num = index
-                                self.panel.set_text("", shape.info)
+                                self.panel.set_text("选择的图形形状：" + shape.get_shape(), shape.info)
                                 break
                 # 鼠标单击右键
                 elif event.button == 3:
@@ -115,7 +127,8 @@ class SDLThread:
                 pos = pygame.mouse.get_pos()
                 if self.state == 1 and pos[1] < self.size[1]:
                     self.add_point(pos)
-
+            if self.current_num < len(self.shapes):
+                self.shapes[self.current_num].info = self.panel.get_input()
             self.draw()
 
     def start(self):
@@ -137,11 +150,12 @@ class MyFrame(wx.Frame):
     def __init__(self, parent, panel_id, title, panel_size, win_size):
         wx.Frame.__init__(self, parent, panel_id, title, size=win_size)
         self.SetMaxSize(win_size)
-        self.input_box = wx.TextCtrl(self, -1, '', pos=(310, 610), size=(200, 30))
-        self.type_box = wx.TextCtrl(self,  -1, '', pos=(110, 610), size=(100, 30))
+        self.choose_button = wx.Button(self, label="选择", pos=(10, 610), size=(70, 30))
+        self.draw_button = wx.Button(self, label="绘图", pos=(90, 610), size=(70, 30))
+        self.identify_button = wx.Button(self, label="识别", pos=(170, 610), size=(70, 30))
+        self.input_box = wx.TextCtrl(self, -1, '', pos=(250, 610), size=(210, 30))
+        self.type_box = wx.TextCtrl(self,  -1, '', pos=(0, 650), size=(win_size[0], 30))
         self.type_box.SetEditable(False)
-        self.identify_button = wx.Button(self, label="识别", pos=(10, 610), size=(80, 30))
-        self.choose_button = wx.Button(self, label="选择", pos=(10, 650), size=(80, 30))
         self.pnlSDL = SDLPanel(self, -1, panel_size)
 
     def get_input(self):
@@ -154,7 +168,7 @@ class MyFrame(wx.Frame):
 
 def main():
     app = wx.App()
-    frame = MyFrame(None, wx.ID_ANY, "PY画板", (800, 600), (810, 740))
+    frame = MyFrame(None, wx.ID_ANY, "PY画板", (800, 600), (810, 715))
     frame.Show()
     app.MainLoop()
 
