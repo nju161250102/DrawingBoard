51b23c7494507b7753889e1cb38197df97851eb2
Qian <161250102@smail.nju.edu.cn>
2018-09-13 17:15:54 +0800

    合并正方形和长方形并用点列表存储

--- Model.py
+++ Model.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 import pygame
-import math
+import Tools
 
 
 class Shape(object):
@@ -30,46 +30,27 @@ class Triangle(Shape):
         return "三角形"
 
     def judge_point(self, point):
-        t1 = self._cross_product(point, self.points[0], self.points[1])
-        t2 = self._cross_product(point, self.points[1], self.points[2])
-        t3 = self._cross_product(point, self.points[2], self.points[0])
-        return (t1 < 0 and t2 < 0 and t3 < 0) or (t1 > 0 and t2 > 0 and t3 > 0)
-
-    @staticmethod
-    def _cross_product(p, point_a, point_b):
-        return (point_a[0] - p[0])*(point_b[1] - p[1]) - (point_a[1] - p[1])*(point_b[0] - p[0])
+        x, y = Tools.decompose(self.points[1], self.points[2], point, origin=self.points[0])
+        return 0 < x < 1 and 0 < y < 1 and 0 < x + y < 1
 
 
 class Rect(Shape):
-    def __init__(self, pos, size, color, info):
+    def __init__(self, points, color, info):
         Shape.__init__(self, color, info)
-        self.pos = pos  # (left, top)
-        self.size = size  # (width, height)
+        self.points = points
 
     def draw(self, screen, active):
-        pygame.draw.rect(screen, self.color, pygame.rect.Rect(self.pos, self.size), 3 if active else 1)
+        pygame.draw.lines(screen, self.color, True, self.points, 3 if active else 1)
 
     def get_shape(self):
-        return "矩形"
+        if Tools.get_distance(self.points[0], self.points[1]) - Tools.get_distance(self.points[0], self.points[3]) < 1:
+            return "正方形"
+        else:
+            return "长方形"
 
     def judge_point(self, point):
-        return (0 < point[0] - self.pos[0] < self.size[0]) and (0 < point[1] - self.pos[1] < self.size[1])
-
-
-class Square(Shape):
-    def __init__(self, pos, side, color, info):
-        Shape.__init__(self, color, info)
-        self.pos = pos
-        self.side = side
-
-    def draw(self, screen, active):
-        pygame.draw.rect(screen, self.color, pygame.rect.Rect(self.pos, (self.side, self.side)), 3 if active else 1)
-
-    def get_shape(self):
-        return "正方形"
-
-    def judge_point(self, point):
-        return (0 < point[0] - self.pos[0] < self.side) and (0 < point[1] - self.pos[1] < self.side)
+        x, y = Tools.decompose(self.points[1], self.points[3], point, origin=self.points[0])
+        return 0 < x < 1 and 0 < y < 1
 
 
 class Circle(Shape):
@@ -85,4 +66,4 @@ class Circle(Shape):
         return "圆形"
 
     def judge_point(self, point):
-        return math.sqrt((point[0] - self.pos[0])**2 + (point[1] - self.pos[1])**2) < self.radius
+        return Tools.get_distance(point, self.pos) < self.radius
